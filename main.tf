b004cd580d2b29048a634cbebfef786fdb2de5a1
mdshaik851 <md939255shaik@gmail.com>
2025-06-16 10:03:59 +0000

    modified changes

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.0"
+ }
+
+  }
+  required_version = ">= 1.10.0"
+}
+
+
+
+
 module "vpc" {
   source = "./modules/vpc"
   region = var.region
--- variables.tf
+++ variables.tf
@@ -1,27 +1,34 @@
 variable "region" {
+  type = string
   default = "us-west-1"
 }
 
 variable "vpc_cidr" {
+  type = string
   default = "10.0.0.0/16"
 }
 
 variable "public_subnet" {
+  type = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "availability_zone" {
+  type = list(string)
   default     = ["us-west-1a", "us-west-1b"]
 }
 
 variable "private_subnet" {
+  type = list(string)
   default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "instance_type" {
+  type = string
   default     = "t2.medium"
 }
 
 variable "ami_id" {
+  type = string
   default = "ami-014e30c8a36252ae5"
 }
